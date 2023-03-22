891ab7a01426018a1b9b31b8ff0399a303af8e32
essadike elhafiane <eelhafia@student.1337.ma>
2023-03-22 23:24:33 +0000

    alphabet_X10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function write alphabet
+ *
+ * Description: Prints a string
+ *
+ * Return: function return nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+}
