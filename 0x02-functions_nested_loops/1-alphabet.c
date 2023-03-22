0718d25380b44477967d75d90519fe2d4962bfe1
essadike elhafiane <eelhafia@student.1337.ma>
2023-03-22 23:17:02 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function write alphabet
+ *
+ * Description: Prints a string
+ *
+ * Return: function return nothing
+ */
+
+void print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 28);
+	return ;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 
 #include <unistd.h>
 int _putchar(char c);
+void print_alphabet(void);
 #endif
