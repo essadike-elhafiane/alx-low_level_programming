3ec8d25736a0138276d231bba7dcfcffe4ed239d
essadike elhafiane <eelhafia@student.1337.ma>
2023-03-24 02:47:17 +0000

    cc

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,8 +5,9 @@
 */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i;
+
+    i = 0;
+    positive_or_negative(i);
+    return (0);
 }
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,13 +5,10 @@
 */
 int main(void)
 {
-int i;
-printf("Infinite loop incoming :(\n");
-i = 0;
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
+    int i;
+
+    printf("Infinite loop incoming :(\n");
+    i = 0;
+    printf("Infinite loop avoided! \\o/\n");
+    return (0);
 }
