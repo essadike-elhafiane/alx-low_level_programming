624b0f483feac759317b4775b3b9ac8695557db1
essadike elhafiane <eelhafia@student.1337.ma>
2023-04-03 21:03:19 +0000

    1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /*
-* _memset : function that take a string and set c in string 
-*@b : is pointer to string
-*@c : caracter
-*@len : is size of bit set in string
-*return : return string 
+* _memset - function that take a string and set c in string 
+* @b: is pointer to string
+* @c: caracter
+* @len: is size of bit set in string
+*
+* return: return string 
 */
 
 void *_memset(void *b, int c, size_t len)
